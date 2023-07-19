775b822764233c5ffa291260fc3558b1ce34271f
JiaLe <2840245168@qq.com>
2023-07-20 00:40:38 +0800

    V0.0.6(优化短信登录模板，完成短信登录功能)

--- Web/views/views.py
+++ Web/views/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django import forms
 from Web import models
 from utils.encrypt import md5
@@ -11,7 +11,7 @@ class LoginForm(forms.Form):
     # 身份验证, 可选字段使用
     role = forms.ChoiceField(
         required=True,
-        choices=(("1", "管理员"), ("2", "客户")),
+        choices=(("1", "管理员"), ("2", "用户")),
         # 下拉框使用select
         widget=forms.Select(attrs={"class": "form-control"})
     )
@@ -29,8 +29,28 @@ class LoginForm(forms.Form):
     )
 
 
-def login(request):
+class SmsLoginForm(forms.Form):
+    role = forms.ChoiceField(
+        required=True,
+        choices=(("1", "管理员"), ("2", "用户")),
+        # 下拉框使用select
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
+    mobile = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "手机号"})
+    )
+    code = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "验证码"})
+    )
 
+
+# 1 管理员 2客户
+mapping = {"1": "ADMIN", "2": "CUSTOMER"}
+
+
+def login(request):
     # 如果请求方法是POST，则返回渲染的HTML模板，即LoginForm表单会生成html代码，供用户输入数据并提交
     if request.method == "GET":
         form = LoginForm()  # 创建一个空的LoginForm表单对象，此时用户还未输入任何数据，所以它的字段当前都为空
@@ -49,8 +69,7 @@ def login(request):
     password = form.cleaned_data.get("password")
     md5_password = md5(password)  # 密码加密
 
-    # 去数据库校验数据 1 管理员 2客户
-    mapping = {"1": "ADMIN", "2": "CUSTOMER"}
+    # 去数据库校验数据
     if role not in mapping:
         return render(request, "login.html", {"form": form, "error": "角色不存在"})
 
@@ -62,17 +81,41 @@ def login(request):
     if not user_object:
         return render(request, "login.html", {"form": form, "error": "用户名或密码错误"})
 
-    request.session["user_info"] = {"role": mapping[role], "name": user_object.username, "id": user_object.id}
+    # request.session["user_info"] = {"role": mapping[role], "name": user_object.username, "id": user_object.id}
 
-    return redirect("/首页/")
+    return HttpResponse(request, "登录成功")
 
 
 def sms_login(request):
-    mobile = request.POST.get("mobile")
-    print(mobile)
-    auth_code = request.POST.get("auth_code")
-    print(auth_code)
-    code = request.POST.get("code")
-    print(code)
+    if request.method == "GET":
+        form = SmsLoginForm()
+        return render(request, "sms_login.html", {"form1": form})
 
-    return render(request, "sms_login.html")
+    form = SmsLoginForm(data=request.POST)
+
+    if not form.is_valid():
+        return render(request, "sms_login.html", {"form1": form, "error": "请输入手机号或验证码！"})
+
+    role = form.cleaned_data.get("role")
+    mobile = form.cleaned_data.get("mobile")
+    code = form.cleaned_data.get("code")
+
+    if role not in mapping:
+        return render(request, "sms_login.html", {"form1": form, "error": "角色不存在"})
+
+        # 去数据库校验数据
+    if role not in mapping:
+        return render(request, "sms_login.html", {"form1": form, "error": "角色不存在"})
+
+    if role == "1":
+        user_object = models.Administrator.objects.filter(active=1, mobile=mobile).first()
+    else:
+        user_object = models.Customer.objects.filter(active=1, mobile=mobile).first()
+
+    if not user_object:
+        return render(request, "sms_login.html", {"form1": form, "error": "当前号码未注册！"})
+
+    if code == "1111":
+        # request.session["user_info"] = {"role": mapping[role], "name": user_object.mobile}
+        return HttpResponse(request, "登录成功")
+        # return redirect("/首页/")
